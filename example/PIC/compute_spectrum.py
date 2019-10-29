a20a21c8e0476c07123d178a4aea739212483445
Andrei Berceanu <andreicberceanu@gmail.com>
2019-10-29 20:27:18 +0100

    Minor fixes

--- example/PIC/compute_spectrum.py
+++ example/PIC/compute_spectrum.py
@@ -4,20 +4,19 @@ from synchrad.calc import SynchRad
 
 if __name__ == "__main__":
     with h5py.File("tracks.h5", "r") as f:
-        time_step = f["misc/cdt"][()]
+        time_step = f["misc/cdt"][...]
 
     calc_input = {
         "grid": [(1.0, 0.6e5), (0, 0.04), (0.0, 2 * np.pi), (256, 32, 32)],
         "timeStep": time_step,
         "dtype": "double",
         "native": True,
-        # "ctx": "mpi",
     }
     calc = SynchRad(calc_input)
 
     with h5py.File("tracks.h5", "r") as f:
         calc.calculate_spectrum(h5_file=f)
 
-    if calc.comm.rank == 0:
+    if calc.rank == 0:
         with h5py.File("spectrum.h5", "w") as f:
             f["radiation"] = calc.Data["radiation"]
