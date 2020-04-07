changed coordinate units to meters according to openPMD update updated examples bug fix

example/PIC/compute_spectrum.py
@@ -12,14 +12,14 @@ if __name__ == "__main__":
     it_range = (it_start_glob, it_end_glob)
 
     calc_input = {
-        "grid": [ (10, 1.2e4),
+        "grid": [ (10 * 1e6, 1.2e4 * 1e6),
                   (0, 0.03),
                   (0.0, 2 * np.pi),
                   (256, 36, 36)],
         "timeStep": time_step,
         "dtype": "double",
         "native": False,
-        "ctx": 'mpi',
+        # "ctx": 'mpi',
     }
 
     calc = SynchRad(calc_input)

synchrad/calc.py
@@ -360,7 +360,11 @@ class SynchRad(Utilities):
             self.Data['radiation'][key] = np.ascontiguousarray(buff, dtype=np.double)
 
     def _track_to_device(self, particleTrack):
-        x, y, z, ux, uy, uz, wp, it_start = particleTrack
+        if len(particleTrack) == 8:
+            x, y, z, ux, uy, uz, wp, it_start = particleTrack
+        elif len(particleTrack) == 7:
+            x, y, z, ux, uy, uz, wp = particleTrack
+            it_start = 0
 
         x = arrcl.to_device( self.queue,
             np.ascontiguousarray(x.astype(self.dtype)) )

synchrad/utils.py
@@ -258,7 +258,7 @@ def tracksFromOPMD(ts, pt, ref_iteration, fname=None, species=None,
     iterations = iterations[iter_ind_select]
     Nt = iterations.size
 
-    dt = (ts.t[1] - ts.t[0]) * c * 1e6 # in microns as coordinates
+    dt = (ts.t[1] - ts.t[0]) * c
 
     tracks = np.zeros( (Nt, Np_select, 7), dtype=np.double )
     nsteps = np.zeros( Np_select, dtype=np.int )