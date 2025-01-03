dc1b23a1723bd2c82b35f2d247ca759856ea2d2a
Igor Andriyash <igor.andriyash@gmail.com>
2025-01-03 16:03:14 +0100

    Update calc.py

--- synchrad/calc.py
+++ synchrad/calc.py
@@ -514,7 +514,7 @@ class SynchRad(Utilities):
                                                    np.sin(self.Args['phi']) )
             self.Data['cosPhi'] = arrcl.to_device( self.queue,
                                                    np.cos(self.Args['phi']) )
-    def _init_comm(self, verbose=False):
+    def _init_comm(self, verbose=True):
 
         ctx_kw_args = {}
 
