5740e65c01cc1488956c8d327d2df55bd01b55f3
Igor Andriyash <igor.andriyash@gmail.com>
2025-10-22 14:44:05 +0200

    minor fix and improvement in PIC tutorial

--- tutorials/PIC/compute_spectrum.py
+++ tutorials/PIC/compute_spectrum.py
@@ -1,12 +1,15 @@
 import numpy as np
 from synchrad.calc import SynchRad
+from synchrad.utils import energy_1m_eV
+
 
 if __name__ == "__main__":
     calc_input = {
-        "grid": [ (10 * 1e6, 1.2e4 * 1e6),
+        "grid": [ (10.0 / energy_1m_eV, 10e3 / energy_1m_eV), # from 10eV to 10keV
                   (0, 0.03),
                   (0.0, 2 * np.pi),
                   (256, 36, 36)],
+        "Features":['logGrid',], # to plot log-scale x-axis later
         "dtype": "double",
         "native": False,
         # "ctx": [0, 0], set your context to avoid being asked
